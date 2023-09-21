a6890fb5225795c113ad48edd01fea3419b04c8f
Hyan Ferreira <114369661+HyanFerreira@users.noreply.github.com>
2023-09-20 21:14:09 -0300

    20/09 - implementação do js/login

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from blog.views import index, login
 
 urlpatterns = [
     path('', index, name= "index"),
+    path('home/', index, name= "index"),
     path('login/', login, name= "login"),
 ]
--- static/js/base.js
+++ static/js/base.js
@@ -4,11 +4,32 @@ let registerContainer = document.querySelector('.register-container');
 let loginContainer = document.querySelector('.login-container');
 
 btnARegister.addEventListener('click', () => {
+    registerContainer.style.transform = "translateX(0)";
     registerContainer.style.opacity = "1";
     loginContainer.style.opacity = "0";
+    loginContainer.style.transform = "translateX(-100%)";
 });
 
 btnALogin.addEventListener('click', () => {
-    loginContainer.style.opacity = "1";
+    registerContainer.style.transform = "translateX(100%)";
     registerContainer.style.opacity = "0";
+    loginContainer.style.opacity = "1";
+    loginContainer.style.transform = "translateX(0)";
+});
+
+document.getElementById("form-login").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    let user = document.getElementById("email-login").value;
+    let password = document.getElementById("password-login").value;
+
+    if (user === "admin@admin" && password === "admin") {
+        window.location.href = "/home/";
+    } else if (user === "" && password === "") {
+        document.getElementById("massage-error").innerHTML =
+            "Por favor, preencha todos os campos!";
+    } else {
+        document.getElementById("massage-error").innerHTML =
+            "Usuário ou senha incorreto!";
+    }
 });
