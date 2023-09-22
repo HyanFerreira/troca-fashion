072a0dd74d7a384b4a6bbdae5fe8f8b3b2900055
Hyan Ferreira <114369661+HyanFerreira@users.noreply.github.com>
2023-09-21 21:14:47 -0300

    21/09 - layout header e footer

--- static/js/index.js
+++ static/js/index.js
@@ -3,6 +3,6 @@ let aside = document.querySelector(".aside-menu");
 let container = document.querySelector(".container");
 
 btnLateral.addEventListener('click', () => {
-    aside.style.marginLeft = "-500px";
-    container.style.marginLeft = "0";
+    aside.classList.toggle("active");
+    container.classList.toggle("active");
 });
