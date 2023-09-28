de3521c6b3438a41cdbb9b749de08a9985b63946
Hyan Ferreira <hcgamesmg2016@gmail.com>
2023-09-28 09:32:57 -0300

    Desenvolvimento page index

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,9 @@
-let btnLateral = document.querySelector(".btn");
+const hamburguer = document.querySelector(".hamburguer");
 let aside = document.querySelector(".aside-menu");
 let container = document.querySelector(".container");
 
-btnLateral.addEventListener('click', () => {
+hamburguer.addEventListener("click", () => {
+    hamburguer.classList.toggle('active');
     aside.classList.toggle("active");
     container.classList.toggle("active");
 });
