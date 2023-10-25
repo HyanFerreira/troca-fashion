fde97777fc2d49503b66c4df729fb556fd86a1f6
Hyan Ferreira <hcgamesmg2016@gmail.com>
2023-10-25 10:18:30 -0300

    updating the general structure of the site

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from blog.views import index, login
-
-urlpatterns = [
-    path('', index, name= "index"),
-    path('home/', index, name= "index"),
-    path('login/', login, name= "login"),
-]
